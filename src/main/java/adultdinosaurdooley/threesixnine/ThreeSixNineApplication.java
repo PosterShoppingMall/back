a95500025b27a5d97823d47b2ad32a08ec130005
eumssooo <mamma1115@gmail.com>
2023-09-29 03:00:41 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/adultdinosaurdooley/threesixnine/ThreeSixNineApplication.java
@@ -0,0 +1,13 @@
+package adultdinosaurdooley.threesixnine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeSixNineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeSixNineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/adultdinosaurdooley/threesixnine/ThreeSixNineApplicationTests.java
@@ -0,0 +1,13 @@
+package adultdinosaurdooley.threesixnine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeSixNineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
